523163a0f02bc0528713c04d8c985970943f81c4
Vanna Winland <vgwinland@gmail.com>
2019-09-10 21:13:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(number)
-  counter = 10 
   
-  while counter > 0 
+  while number > 0 
   puts "#{number} SECOND(S) !"
-  counter -= 1 
+  number -= 1 
 end
 
 puts "HAPPY NEW YEAR !"
